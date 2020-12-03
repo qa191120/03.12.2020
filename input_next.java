92b4ceae4d69ac6bb3588b61710314f7854d3971
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-12-03 18:28:45 +0200

    Create input_next.java

---
+++ input_next.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in); // chinese
+            System.out.println("Enter integer number x:");
+            //System.out.println();
+
+            long savings = 0;
+            // return value
+            int x = scanner.nextInt(); // why ( ) ? -- function operation
+                                       // Error -- in red!
+
+            // 1. input from user 2 int numbers into a,b
+            // 2. print a * b
+            // 3. input from user 2 double numbers into c,d (nextDouble)
+            // 4. print c / d
+            // 5. input from user 2 longs into e, f
+            // 6. print a + b * c - d + (e * f)
+            
+            System.out.println("Finished... see you soon...");
+
+
+    }
+}
